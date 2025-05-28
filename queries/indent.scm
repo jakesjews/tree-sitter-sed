141ec8c6ebca3e85d9aa40f03f01e2c322a4779c
Jacob Jewell <jakesjews@gmail.com>
2025-05-28 03:38:06 -0400

    update indent

--- queries/indent.scm
+++ queries/indent.scm
@@ -1,2 +1,8 @@
-((text_block)     @indent.begin)
+;; A   text_block     (a\ …  c\ …  i\ …)
+((text_block)      @indent.begin)
+((text_block)      @indent.end)
+
+;; A multi-line command continued with “\⏎”
 ((command_payload) @indent.begin)
+((command_payload) @indent.end)
+
